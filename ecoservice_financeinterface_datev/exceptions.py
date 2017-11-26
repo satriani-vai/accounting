d32563ea72fa584cd1ba3aa34239197117717301
Alex <satriani-vai@users.noreply.github.com>
2017-11-26 03:23:20 +0100

    Update exceptions.py

--- ecoservice_financeinterface_datev/exceptions.py
+++ ecoservice_financeinterface_datev/exceptions.py
@@ -16,11 +16,11 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    This program based on OpenERP.
-#    OpenERP, Open Source Management Solution
+#    This program based on Odoo.
+#    Odoo, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 
-from openerp.addons.ecoservice_financeinterface.exceptions import FinanceinterfaceException
+from odoo.addons.ecoservice_financeinterface.exceptions import FinanceinterfaceException
 
 
 class DatevWarning(FinanceinterfaceException):
