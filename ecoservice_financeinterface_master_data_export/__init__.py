5a89eac0e9c0e813b91a15b2313140d8c71f2b91
Alex <satriani-vai@users.noreply.github.com>
2017-11-26 03:37:12 +0100

    Update __init__.py

--- ecoservice_financeinterface_master_data_export/__init__.py
+++ ecoservice_financeinterface_master_data_export/__init__.py
@@ -16,8 +16,8 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    This program based on OpenERP.
-#    OpenERP, Open Source Management Solution
+#    This program based on Odoo.
+#    Odoo, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 ##############################################################################
 import models
