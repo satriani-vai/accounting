faf32cdca69e376d282e50e3167562c0f3746741
Alex <satriani-vai@users.noreply.github.com>
2017-11-26 03:21:58 +0100

    Update ecoservice_financeinterface_datev_installer.py

--- ecoservice_financeinterface_datev/ecoservice_financeinterface_datev_installer.py
+++ ecoservice_financeinterface_datev/ecoservice_financeinterface_datev_installer.py
@@ -18,13 +18,13 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    This program based on OpenERP.
-#    OpenERP, Open Source Management Solution
+#    This program based on Odoo.
+#    Odoo, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 ##############################################################################
 
-from openerp.osv import fields, osv
-from openerp import SUPERUSER_ID
+from odoo.osv import fields, osv
+from odoo import SUPERUSER_ID
 import socket
 import fcntl
 import struct
